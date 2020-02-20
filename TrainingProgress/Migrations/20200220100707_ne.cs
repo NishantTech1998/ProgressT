f86fb8fe3c4e01318fc5c314979a9f272f3e3b35
Ajay RN <ajay94rn@gmail.com>
2020-02-20 15:37:38 +0530

    2.0

---
+++ TrainingProgress/Migrations/20200220100707_ne.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrainingProgress.Migrations
+{
+    public partial class ne : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TaskName = table.Column<string>(nullable: true),
+                    StartTime = table.Column<DateTime>(nullable: false),
+                    EndTime = table.Column<DateTime>(nullable: false),
+                    TaskDate = table.Column<DateTime>(nullable: false),
+                    IsPlanned = table.Column<bool>(nullable: false),
+                    IsPaused = table.Column<bool>(nullable: false),
+                    TotalTimeTaken = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
---
+++ TrainingProgress/TaskProgressContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace TrainingProgress
+{
+    public class TaskProgressContext:DbContext
+    {
+        public DbSet<Tasks> Tasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=./task.db");
+        }
+    }
+}
