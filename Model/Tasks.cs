839f1fbf5111a0044eaaf6bb2415b7f61c482c9a
Ajay RN <ajay94rn@gmail.com>
2020-02-20 15:33:07 +0530

    commit 1.0

---
+++ Model/Tasks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Tasks
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public DateTime TaskDate { get; set; }
+        public bool IsPlanned { get; set; }
+        public bool IsPaused { get; set; }
+        public DateTime TotalTimeTaken { get; set; }
+
+    }
+}
